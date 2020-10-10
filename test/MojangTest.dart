5e2855d3fe2c89bc7c70154b8e78a0fbd1487000
pnda <43609023+ThePNDA@users.noreply.github.com>
2020-10-10 08:28:38 +0200

    Prepare for publish

--- test/MojangTest.dart
+++ test/MojangTest.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:dart_minecraft/src/Mojang/MojangStatus.dart';
 import 'package:test/test.dart';
-import 'package:dart_minecraft/minecraft.dart';
+import 'package:dart_minecraft/dart_minecraft.dart';
 
 void main() {
   final test_data = json.decode(File('./test_data.json').readAsStringSync());
