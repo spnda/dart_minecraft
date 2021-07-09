71d9dfc659642a0b85e4395e6c003a76d834b208
spnda <43609023+spnda@users.noreply.github.com>
2021-07-09 17:13:43 +0200

    Refactor: Remove server_ping.dart and export from main lib

--- lib/dart_minecraft.dart
+++ lib/dart_minecraft.dart
@@ -38,7 +38,8 @@ export 'src/packet/packet_compression.dart';
 export 'src/packet/packet_reader.dart';
 export 'src/packet/packet_writer.dart';
 export 'src/packet/packets/server_packet.dart';
-export 'src/ping/server_ping.dart';
+export 'src/ping/server_ping_stub.dart'
+  if (dart.library.io) 'src/ping/server_ping_io.dart';
 export 'src/utilities/pair.dart';
 export 'src/yggdrasil.dart';
 export 'src/yggdrasil_api.dart';
--- lib/src/ping/server_ping.dart
+++
@@ -1,1 +0,0 @@
-export 'server_ping_stub.dart' if (dart.library.io) 'server_ping_io.dart';
